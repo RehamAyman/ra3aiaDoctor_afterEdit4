c619e9150935371ec4840fe1b71f65f6d597de50
reham ayman <rehamayman15@icloud.com>
2021-08-16 16:43:39 +0200

    storyboard edit 1

---
+++ Ra3ia/DoctorModel/AuthnticationResponse/AuthResponse/anmilsModel.swift
@@ -0,0 +1,22 @@
+//
+//  anmilsModel.swift
+//  Ra3ia
+//
+//  Created by Sara Mady on 04/04/2021.
+//
+
+import Foundation
+
+// MARK: - FillterdDoctors
+struct Anmils: Codable ,CodableInit{
+    let key: String
+    let code: Int
+    let data: [anmil]
+}
+
+// MARK: - Datum
+struct anmil: Codable {
+    let id: Int
+    let name: String
+    var isSelected:String?
+}
